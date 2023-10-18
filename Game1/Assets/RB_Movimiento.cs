16da80afed57973ec7022ba10e90e524526bc747
lorca03 <guillermolorcamartinez@gmail.com>
2023-10-19 00:41:26 +0200

    Jump

--- Game1/Assets/RB_Movimiento.cs
+++ Game1/Assets/RB_Movimiento.cs
@@ -8,43 +8,38 @@ public class RB_Movimiento : MonoBehaviour
     private Vector3 movedirection;
     private Rigidbody rb;
     public float speed;
-    private PlayerInput inputPlayer;
     public float jumpForce;
+    private PlayerInput inputPlayer;
     RaycastHit hitInfo;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         inputPlayer = GetComponent<PlayerInput>();
-        inputPlayer.actions["Jump"].started += RB_Movimiento_started;
-
-    }
-
-    private void RB_Movimiento_started(InputAction.CallbackContext obj)
-    {
-        if (hitInfo.transform.name == "Suelo")
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Force);
-
-            
-        }
+        inputPlayer.actions["Jump"].performed += RB_Movimiento_started;
     }
 
     private void Update()
     {
-
-        Debug.Log(rb.velocity.y);
-        Physics.Raycast(transform.position, Vector3.down, out hitInfo, 3);
-
-        Debug.DrawRay(transform.position, Vector3.down * 3, Color.green);
+        //Debug.Log(rb.velocity.y);
         Input();
-        movedirection = new Vector3(Input().x * speed , rb.velocity.y, 0);
-       // Debug.Log(hitInfo.transform.name);
+        movedirection = new Vector3(Input().x * speed , rb.velocity.y, 0f);
     }
 
     void FixedUpdate()
     {
         rb.velocity = movedirection;
+    }
+
+    private void RB_Movimiento_started(InputAction.CallbackContext obj)
+    {
+        float altura = GetComponent<Renderer>().bounds.size.y / 2 + 0.2f;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, altura))
+        {
+            Debug.Log(rb.velocity.y);
+            Debug.Log("Jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     private Vector2 Input()
