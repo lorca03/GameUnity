15b8f1f8ae9ab4bded79ec6db279bef6bf9b9381
lorca03 <guillermolorcamartinez@gmail.com>
2023-12-21 13:40:52 +0100

    Cambio Menu

---
+++ Game1/Assets/Canvi_color_text_button.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Canvi_color_text_button : MonoBehaviour
+{
+
+    public TMP_Text textMeshPro;
+    Color color;
+
+    private void Start()
+    {
+        color = textMeshPro.color;
+    }
+
+    public void Color_Neon()
+    {
+        textMeshPro.color = new Color(0,253,255);
+
+    }
+
+    public void Color_Origirnal()
+    {
+        textMeshPro.color = color;
+    }
+
+
+}
---
+++ Game1/Assets/RotarSkybox.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotarSkybox : MonoBehaviour
+{
+    public float velocidadRotacion = 1f;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * velocidadRotacion);
+    }
+}
