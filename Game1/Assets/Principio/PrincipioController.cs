e7e0aa70c5d21a5eb09358fb2ac9324c65274100
lorca03 <guillermolorcamartinez@gmail.com>
2024-02-05 12:26:04 +0100

    Cambio Skybox y Android

--- Game1/Assets/Principio/PrincipioController.cs
+++ Game1/Assets/Principio/PrincipioController.cs
@@ -8,18 +8,23 @@ public class PrincipioController : MonoBehaviour
     public GameObject player;
     PlayerInput playerInput;
     PlayerController playerController;
+    public GameObject controlesAndroid;
 
     void Start()
     {
         playerInput = player.GetComponent<PlayerInput>();
         playerController = player.GetComponent<PlayerController>();
+        controlesAndroid.SetActive(false);
     }
 
     public void EmpezarJuego()
     {
-        Debug.Log("empieza");
         playerInput.SwitchCurrentActionMap("InputsPlayer");
         Camera.main.GetComponent<AudioSource>().mute = false;
         StartCoroutine(playerController.Contador());
+        if (Input.touchSupported)
+        {
+            controlesAndroid.SetActive(true);
+        }
     }
 }
